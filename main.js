9dc77a291fcf43c4acf9ec056b60c9b8c30ba86d
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-30 15:29:06 +0100

    Inizio esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+// 1. creare un oggetto che rappresenta uno studente: ogni studente è caratterizzato da un nome, un cognome e un'età. Attraverso un ciclo for-in stampare a schermo tutte le sue proprietà e il relativo valore
+// 2. creare un array di oggetti "studente". Ciclare su tutti gli studenti e stampare per ciascuno il nome e il cognome
+// 3. tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente. Creare con questi dati un nuovo oggetto e inserirlo nell'array del punto precedente.
+// Nome repo: js-oggetti-studenti
+
+$(document).ready(function() {
+
+});
