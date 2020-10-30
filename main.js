8743c78b257cfcdf1cca945157c35d106a52e895
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-30 17:31:11 +0100

    secondo esercizio

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-// 2. creare un array di oggetti "studente". Ciclare su tutti gli studenti e stampare per ciascuno il nome e il cognome
 // 3. tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente. Creare con questi dati un nuovo oggetto e inserirlo nell'array del punto precedente.
 // Nome repo: js-oggetti-studenti
 
@@ -15,5 +13,30 @@ $(document).ready(function() {
     for (var key in studente) {
         console.log(studente[key]);
     }
+
+// 2. creare un array di oggetti "studente". Ciclare su tutti gli studenti e stampare per ciascuno il nome e il cognome
+
+    var classe1 = [
+        {
+            'nome': 'Luigi',
+            'cognome': 'Bianchi',
+            'eta': '30',
+        },
+        {
+            'nome': 'Mario',
+            'cognome': 'Rossi',
+            'eta': '27',
+        },
+        {
+            'nome': 'Anna',
+            'cognome': 'Verdi',
+            'eta': '24',
+        },
+    ];
+
+    for (var i = 0; i < classe1.length; i++) {
+        console.log(classe1[i].nome + ' ' + classe1[i].cognome);    
+    }
+
 
 });
