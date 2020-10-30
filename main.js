47d21530cdb412cf7632c1a24f9cffabf57d44db
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-30 17:51:06 +0100

    terzo esercizio

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 3. tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente. Creare con questi dati un nuovo oggetto e inserirlo nell'array del punto precedente.
+// Esercizio Oggetti-studenti JS
 // Nome repo: js-oggetti-studenti
 
 // 1. creare un oggetto che rappresenta uno studente: ogni studente è caratterizzato da un nome, un cognome e un'età. Attraverso un ciclo for-in stampare a schermo tutte le sue proprietà e il relativo valore
@@ -35,8 +35,18 @@ $(document).ready(function() {
     ];
 
     for (var i = 0; i < classe1.length; i++) {
-        console.log(classe1[i].nome + ' ' + classe1[i].cognome);    
+        console.log(classe1[i].nome + ' ' + classe1[i].cognome);
     }
 
+// 3. tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente. Creare con questi dati un nuovo oggetto e inserirlo nell'array del punto precedente.
+
+    var studenteNuovo = {
+        'nome' : prompt('Scrivi il tuo nome'),
+        'cognome' : prompt('Scrivi il tuo cognome'),
+        'eta' : prompt('Quanti anni hai?'),
+    };
+    console.log(studenteNuovo);
+    classe1.push(studenteNuovo);
+    console.log(classe1);
 
 });
